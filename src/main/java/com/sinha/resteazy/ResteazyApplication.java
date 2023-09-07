49c8f2dcb23d3ee641afbc81c9266a7568a4b94d
Aakarsh Sinha <aakarshsak@gmail.com>
2023-09-07 12:16:06 +0530

    Initializing project

---
+++ src/main/java/com/sinha/resteazy/ResteazyApplication.java
@@ -0,0 +1,13 @@
+package com.sinha.resteazy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResteazyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResteazyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sinha/resteazy/ResteazyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sinha.resteazy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResteazyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
